1198e59acca7507dd58511d9f7d2cb0b3d6d7328
LazarusOfSuburbia <alecwgreene@gmail.com>
2020-07-08 11:54:10 -0400

    uhhh everything

--- menu.js
+++ menu.js
@@ -1,8 +1,69 @@
-ScrollableElement($(".n-question-list"));
+// ========== VARIABLES ==========
 
+// ---------- FA Class strings ----------
+/** The FA class for circle */
+var icon_circle_class = "far fa-circle";
+/** The FA class for circled checkmark */
+var icon_circle_check_class = "fas fa-check-circle";
+/** The FA class for a circled cross */
+var icon_circle_cross_class = "fas fa-times-circle";
+/** The FA class for a checkmark */
+var icon_check_class = "fas fa-check";
+/** The FA class for cross */
+var icon_cross_class = "fas fa-times";
+
+// ---------- DOM Elements ----------
+/** The question list */
+var nav_questionList = $(".n-question-list");
+/** The array of questions the user has viewed */
+var nav_questionItem_array = new Array();
+
+// ---------- Global Variables ----------
+/** The starting position of the question list */
 var global_startX = $(".n-question-list").offset().left;
+/** Determines which collections of questions to generate the quiz from */
+var quiz_difficulty = -1;
+/** Determines how long the quiz will be */
+var quiz_length = 0;
 
-function ScrollableElement(a_element){
+
+
+// ========== NAVIGATION FUNCTIONS ==========
+
+/**
+ * Adds a span element to the question list 
+ * 
+ * @param {Int} a_index the index of the question being added
+ * 
+ * @returns {void}
+ */
+function AddQuestionItem(a_index){
+    // Create the span to hold the Question list item
+    var t_element = $("<span>").addClass("n-question-item");
+    $(t_element).attr("data-index", a_index);
+    $(t_element).text("Question " + (a_index + 1) + " ");
+    t_element.on("mouseup", HandleNavItemClick);
+
+    // Add a placeholder for the icon
+    var t_icon = $("<i>").addClass(icon_circle_class);
+    t_element.append(t_icon);
+
+    // Add the item to the question list
+    nav_questionList.append(t_element);
+    nav_questionItem_array.push(t_element);
+}
+
+// TODO: actually call your fucking methods plz
+
+/**
+ * Applies a script to the object which allows the user to drag it along the x-axis using their mouse
+ * 
+ * @param {Element} a_element the element which the user should be able to manipulate
+ * 
+ * @returns {void}
+ */
+function EnableScrollableElement(a_element){
+    /** Variables to help track the mouse and element position */
     var startX = 0, deltaX = 0;
 
     // Add event listener
@@ -15,8 +76,6 @@ function ScrollableElement(a_element){
      */
     function HandleMouseMovement(a_event){
         a_event.preventDefault();
-
-        console.log("Mouse movement detected");
 
         // Grab the mouse position
         startX = a_event.clientX;
@@ -35,21 +94,30 @@ function ScrollableElement(a_element){
      * @returns {void}
      */
     function ScrollElement(a_event){
-        console.log("ScrollElement called");
         a_event.preventDefault();
-        a_event.stopPropagation();
         
+        // Do not trigger the mouseup event on the nav item
+        DisableNavElement(a_event);
+
         // Update position variables
         deltaX = a_event.clientX - startX;
         startX = a_event.clientX;
         
-        if(($(a_element).offset().left + deltaX) > global_startX){
-            $(a_element).offset({top: $(a_element).offset().top, left: $(a_element).offset().left + deltaX});
-        }
-        else{ // Clamp the scroll to the left
-            $(a_element).offset({left: global_startX});
-        }
+        // Shift the nav question list 
+        $(a_element).offset({top: $(a_element).offset().top, left: $(a_element).offset().left + deltaX}); 
         
+    }
+
+    /**
+     * Disables the nav item in the event the user is scrolling the list
+     * 
+     * @param {Event} a_event the mousemove event triggered by n-question-list
+     * 
+     * @returns {void}
+     */
+    function DisableNavElement(a_event){
+        t_item = a_event.target;
+        $(t_item).off("mouseup");
     }
 
     /**
@@ -58,9 +126,194 @@ function ScrollableElement(a_element){
      * @returns {void}
      */
     function StopScrolling(){
-        console.log("Stopped Scrolling");
         // Remove the listeners from the document
         $(document).off("mousemove", ScrollElement);
         $(document).off("mouseup", StopScrolling);
+
+        // Reattach the nav item listener
+        $(t_item).on("mouseup", HandleNavItemClick);
+
+        // Correct the positioning of the elements
+        CorrectFirstVisibleItem(a_element);
     }
 }
+
+/**
+ * Freezes the position of the element by removing the mousedown listeners
+ * 
+ * @param {Element} a_element the scrollable element to freeze the movement of
+ * 
+ * @returns {void}
+ */
+function DisableScrollableElement(a_element){
+    $(a_element).off("mousedown");
+}
+
+/**
+ * Searches through nav_questionItem_array until it finds the leftmost element and shifts a_element array until the next element matches up to the brand
+ * 
+ * @param {Element} a_element
+ * 
+ * @returns {void}
+ */
+function CorrectFirstVisibleItem(a_element){
+    var t_first_element = nav_questionItem_array[0];
+
+    // Loop through the array to find the first element being occluded by the brand
+    for(let i = 0; i < nav_questionItem_array.length; i++){
+        // If the ith item is to the right of the global_startX then break the loop
+        if($(nav_questionItem_array[i]).offset().left > global_startX){
+            break;
+        }
+        // If the ith item is to the left of the global_startX then assign that to t_first_element
+        else if($(nav_questionItem_array[i]).offset().left <= global_startX){
+            t_first_element = nav_questionItem_array[i];
+        }
+    }
+
+    // Create a variable to store how far to shift the element and it's current position
+    var t_shift = 0;
+    var t_position = $(a_element).offset().left;
+
+    // If the first element's distance to global_startX is smaller than half the element's width
+    if((global_startX - $(t_first_element).offset().left) < 0.8 * $(t_first_element).width()){
+        t_shift = global_startX - $(t_first_element).offset().left;
+    }
+    else{
+        // Grab the next element in the question array
+        var t_next_index = $(t_first_element).data("index") + 1;
+        t_first_element = nav_questionItem_array[t_next_index];
+
+        // Store the value
+        t_shift = global_startX - $(t_first_element).offset().left;
+    }
+
+    // Shift the element
+    t_position += t_shift;
+    $(a_element).offset({left: t_position});
+}
+
+/**
+ * Displays the question corresponding to the index of the clicked nav item
+ * 
+ * @param {Event} a_event The event of a nav item being clicked
+ * 
+ * @returns {void}
+ */
+function HandleNavItemClick(a_event){
+    a_event.preventDefault();
+
+    // Display the question
+    DisplayQuestion(questionArray[$(a_event.target).data("index")]);
+}
+
+
+
+
+// ========== MENU FUNCTIONS ==========
+
+function DisplayMenu(){
+    // Detach all current cards
+    $(".card").remove();
+
+    // Create card DOM element
+    var t_card = $("<div>").addClass("card");
+
+    // Create card title div
+    var t_title = "Welcome to my code quiz\!";
+    var t_card_title = $("<h3>").addClass("card-title").css("text-align","center");
+    $(t_card_title).text(t_title);
+    t_card.append(t_card_title);
+
+    // Create the difficulty and header
+    var t_header_difficulty = $("<h4>").text("Choose a difficulty: ").css("text-align","center").css("font-weight","bold").css("padding-top","2rem");
+    var t_difficulty_span = $("<span>").attr("id","difficulty-span");
+    t_header_difficulty.append(t_difficulty_span);
+
+    // Create the difficulty buttons
+    var t_btn_ul = $("<ul>").addClass("menu-section");
+    var t_btn = $("<button>").addClass("btn btn-success btn-difficulty").attr("data-index",0); $(t_btn_ul).append(t_btn); $(t_btn).text("Easy"); $(t_btn).on("click",HandleMenuClick);
+    t_btn = $("<button>").addClass("btn btn-warning btn-difficulty").attr("data-index",1).css("color","white"); $(t_btn_ul).append(t_btn); $(t_btn).text("Medium"); $(t_btn).on("click",HandleMenuClick);
+    t_btn = $("<button>").addClass("btn btn-danger btn-difficulty").attr("data-index",2); $(t_btn_ul).append(t_btn); $(t_btn).text("Hard"); $(t_btn).on("click",HandleMenuClick);
+    $(t_card).append(t_header_difficulty);
+    $(t_card).append(t_btn_ul);
+
+
+    // Create the length slider
+    var t_header_length = $("<h4>").text("Change the length: ").css("text-align","center").css("font-weight","bold").css("padding-top","2rem");
+    t_header_length.append($("<span>").attr("id","length-span"));
+    var t_slider = $("<input>").attr("type","range").attr("id","length-slider");
+    $(t_slider).attr("max", 50).attr("min", 1);
+    $(t_card).append(t_header_length);
+    $(t_slider).on("input", HandleSliderChange); $(t_card).append(t_slider);
+
+    // Create the Generate button
+    var t_generate_btn = $("<button>").addClass("btn btn-info btn-generate").text("Start Quiz");
+    $(t_generate_btn).css("width","25%").css("margin", "3rem auto 0 auto");
+    $(t_generate_btn).on("click",HandleMenuClick);
+    $(t_card).append(t_generate_btn);
+
+    // Append the menu to the page
+    $("#doc-body").append(t_card);
+
+}
+
+/**
+ * A method to call appropriate functions when a menu button is clicked
+ * 
+ * @param {Event} a_event An event representing a button click
+ * 
+ * @returns {void}
+ */
+function HandleMenuClick(a_event){
+    a_event.preventDefault();
+
+    // Sort the handling methods by the element that called the handler
+    if($(a_event.target).hasClass("btn-difficulty")){ // If the button was a difficulty button
+        quiz_difficulty = $(a_event.target).data("index");
+        $(".btn-difficulty").each(function(a_index, a_element){ 
+            $(a_element).removeClass("active");
+        });
+        $(a_event.target).addClass("active");
+        $("#difficulty-span").text($(a_event.target).text());
+    }
+    else if($(a_event.target).hasClass("btn-generate")){ // If the button was the generate button
+        if(quiz_length > 0 && quiz_difficulty != -1){
+            questionArray = Array.from(GenerateQuiz(quiz_difficulty, quiz_length));
+            
+            for(let i = 0; i < questionArray.length; i++){
+                if(questionArray[i] != undefined){
+                    questionArray[i].m_id = i;
+                    AddQuestionItem(i);
+                }
+                else{
+                    break;
+                }
+            }
+
+            if(questionArray.length > 9){
+                EnableScrollableElement($(".n-question-list"));
+            }
+
+            DisplayQuestion(questionArray[0]);
+        }
+    }
+}
+
+/**
+ * When the slider is changed, change the value of length
+ * 
+ * @param {Event} a_event An event to be called on input from a slider
+ * 
+ * @returns {void} 
+ */
+function HandleSliderChange(a_event){
+    a_event.preventDefault();
+
+    // Set length to the value of the slider
+    quiz_length = $("#length-slider").val();
+    $("#length-span").text(quiz_length);
+}
+
+// TODO: add document.on(ready)
+DisplayMenu();
--- quiz.js
+++ quiz.js
@@ -1,12 +1,27 @@
+// ========== DOM ELEMENTS ==========
+
+
+
+// ========== DISPLAY FUNCTIONS ==========
+
 /**
  * An object representing an object
  * 
- * @property {Array} m_options the options for the user to choose from
+ * @property {string} m_prompt The question prompt
  * 
- * @property {Int} m_answer the correct option for the question
+ * @property {Array<string>} m_options The options for the user to choose from
  * 
- * @property {Int} m_status indicates whether the question was answered correctly or not
+ * @property {Int} m_answer The correct option for the question
  * 
+ * @property {Int} m_status Indicates whether the question was answered correctly or not
+ * 
+ * @property {Int} m_selection Indicates the option that was chosen by the user
+ * 
+ * @property {Int} m_id An idenifier to be assigned when the quiz is generated
+ * 
+ * @property {Int} m_difficulty An integer between 0 and 2 representing the difficulty of the question
+ * 
+ * @method CollectAnswer Collects an answer and changes m_status 
  */
 class Question{
 
@@ -19,9 +34,9 @@ class Question{
      * 
      * @param {Int} a_answer the correct choice
      * 
-     * @returns {void}
+     * @returns {Question} 
     */
-    constructor(a_prompt, a_options, a_answer){
+    constructor(a_prompt, a_options, a_answer, a_difficulty){
         // Throw error if the number of answers is not greater than 1
         if(!(a_options.length > 1)){
             console.log("ERROR: options passed to Question constructor was not greater than 1");
@@ -33,6 +48,7 @@ class Question{
         this.m_status = 0;
         this.m_selection = -1;
         this.m_id = 0;
+        this.m_difficulty = a_difficulty;
     }   
 
     CollectAnswer(a_answer){
@@ -45,25 +61,25 @@ class Question{
             this.m_status = -1;
         }
     }
-
-    /** The user's selection */
-    m_selection = -1;
-    /** The integer representing whether or not the question was answered correctly */
-    m_status = 0;
 }
 
 /**
  * Creates a card using the information from the question
  * 
  * @param {Question} a_question the question the card will represent
+ * 
+ * @returns {void}
  */
 function DisplayQuestion(a_question){
+    // Detach all current cards
+    $(".card").detach();
+
     // Create card DOM element and assign it an id as a data-index
     var t_card = $("<div>").addClass("card");
     t_card.attr("data-index", a_question.m_id);
 
     // Create card title div
-    var t_title = "Question " + a_question.m_id + ": ";
+    var t_title = "Question " + (a_question.m_id + 1) + ": ";
     var t_card_title = $("<h3>").addClass("card-title");
     $(t_card_title).text(t_title + a_question.m_prompt);
     t_card.append(t_card_title);
@@ -86,12 +102,33 @@ function DisplayQuestion(a_question){
     // Append the list to the page
     $(t_card).append(t_option_list);
     $("#doc-body").append(t_card);
+
+    // If the question has been answered then style the list
+    if(a_question.m_status != 0){
+        // Grab the selected element
+        var t_element = $(`.q-options-list li[data-index=${a_question.m_selection}]`);
+
+        // If the question was answered correctly
+        if(a_question.m_status == 1){
+            // Update option element
+            $(t_element).addClass("q-options-correct");
+            $(t_element).append($("<i>").addClass("q-answer-icon " + icon_check_class));
+        }
+        // If the question was answered incorrectly
+        else{
+            // Update option element
+            $(t_element).addClass("q-options-incorrect");
+            $(t_element).append($("<i>").addClass("q-answer-icon " + icon_cross_class));
+        }
+    }
 }
 
 /**
-* Handles the event of a question option being clicked
+* Changes the styling of the selected element and displays the next unanswered question
 * 
 * @param {Event} event the event calling the function containing the data @var a_question and @var a_item
+* 
+* @returns {void}
 */
 function HandleOptionClick(event){
     event.preventDefault();
@@ -100,36 +137,240 @@ function HandleOptionClick(event){
     var t_question = event.data.t_question;
     var t_item = event.data.t_item;
         
-    // Assign the q-options-correct and q-options-incorrect classes 
+    // If the question hasn't been answered
     if(t_question.m_status === 0){
         // Register answer
          t_question.CollectAnswer($(t_item).data("index"));
 
-        // Add the appropriate the classes
+        // If the question was answered correctly
          if(t_question.m_status === 1){
+            // Update option styling
             $(t_item).addClass("q-options-correct");
             $(t_item).append($("<i>").addClass("q-answer-icon " + icon_check_class));
+
+            // Update nav element
+            var t_element = GetNavItem(t_question.m_id);
+            $(t_element).children("i").removeClass(icon_circle_class);
+            $(t_element).children("i").addClass(icon_circle_check_class);
         }
+        // If the question was answered incorrectly
         else if(t_question.m_status === -1){
+            // Update option element
             $(t_item).addClass("q-options-incorrect");
             $(t_item).append($("<i>").addClass("q-answer-icon " + icon_cross_class));
+            
+            // Update nav element
+            var t_element = GetNavItem(t_question.m_id);
+            $(t_element).children("i").removeClass(icon_circle_class);
+            $(t_element).children("i").addClass(icon_circle_cross_class);
+        }
+
+        //TODO: fix when user answers last question
+        // Display next unanswered question
+        var t_answered_count = 0;
+        for(let i = t_question.m_id + 1; i < questionArray.length; i++){
+            // If the question hasn't been answered, display it
+            if(questionArray[i].m_status === 0){
+                DisplayQuestion(questionArray[i]);
+                break;
+            }
+            // Else if every question hasn't been checked and the end of the list has been reached
+            else if(t_answered_count != questionArray.length && i === questionArray.length - 1){
+                i = 0;
+            }
+            else{
+                // TODO: put endscreen here
+                break;
+            }
         }
     }
 }
 
-function TestQuestion(){
-    // Create a question
-    var test_question = new Question("This is a test question",["First Answer", "Second Answer", "Correct Answer", "Fourth Answer"],2);
-
-    DisplayQuestion(test_question);
+/**
+ * Retrieves a question item from the list of the navbar
+ * 
+ * @param {Int} a_index The desired index
+ * 
+ * @returns {Element} The element in n-question-list with a matching data-index attribute
+ */
+function GetNavItem(a_index){
+    for(let i = 0; i < nav_questionItem_array.length; i++){
+        if($(nav_questionItem_array[i]).data("index") == a_index){
+            return nav_questionItem_array[i];
+        }
+    }
 }
-TestQuestion();
+
+// ========== GENERATION FUNCTIONS ==========
+
+/**
+ * Selects questions for the quiz using difficulties below the parameter
+ * 
+ * @param {Int} a_difficulty An integer between 0 and 2 representing the difficulty
+ * 
+ * @param {Int} a_number The number of questions to return
+ * 
+ * @returns {Array<Question>} The generated array of questions
+ */
+function GenerateQuiz(a_difficulty, a_number){
+    // Helper arrays to store picked indices
+    var t_questions = new Array(3);
+    var t_chosen_questions = [];
+    t_questions[0] = Array.from(quiz_easyArray);
+    t_questions[1] = Array.from(quiz_mediumArray);
+    t_questions[2] = Array.from(quiz_hardArray);
+
+    // Iterate through the requested number of questions
+    for(let i = 0; i < a_number; i++){
+        // Aleternate between available difficulties
+        var t_choose_difficulty = i % (a_difficulty + 1);
+        /** The question to add to the array */
+        var t_question;
+
+        // Select a random question
+        if(t_choose_difficulty === 0 || (t_questions[2].length === 0 && t_questions[1].length === 0)){
+            // Pick a random index
+            t_index = Math.floor(t_questions[0].length * Math.random());
+
+            // Splice the index out of the array
+            t_question = t_questions[0][t_index];
+            t_questions[0].splice(t_index,1); 
+        }
+        else if(t_choose_difficulty === 1 || t_questions[2].length === 0){
+            // Pick a random index
+            t_index = Math.floor(t_questions[1].length * Math.random());
+
+            // Splice the index out of the array
+            t_question = t_questions[1][t_index];
+            t_questions[1].splice(t_index,1);  
+        }
+        else if(t_choose_difficulty === 2){
+            // Pick a random index
+            t_index = Math.floor(t_questions[0].length * Math.random());
+
+            // Splice the index out of the array
+            t_question = t_questions[2][t_index];
+            t_questions[2].splice(t_index,1); 
+        }
+        else{
+            console.log("ERROR: Chosen difficulty was not a valid value");
+            break;
+        }
+
+        // Push the chosen question to the array
+        t_chosen_questions.push(t_question);
+    }
+
+    // Return the question array
+    return t_chosen_questions;
+}
+
+
+
 
 // ========== VARIABLES ==========
+
+// ---------- Icon Class Strings ----------
 var icon_circle_class = "far fa-circle";
 var icon_circle_check_class = "fas fa-check-circle";
 var icon_circle_cross_class = "fas fa-times-circle";
 var icon_check_class = "fas fa-check";
 var icon_cross_class = "fas fa-times";
 
+// ---------- Global Variables ----------
+/** The questions to give to the user */
+var questionArray = [];
+/** The questions to be inserted into hard quizes */
+var quiz_hardArray = [];
+/** The questions to be inserted into medium quizes */
+var quiz_mediumArray = [new Question("How do you declare an anonymous function?", // Medium Question 0
+                                    ["function(){ \/\/ Code goes here }",
+                                     "function { \/\/ Code goes here }",
+                                     "(){ \/\/ Code goes here }",
+                                     "anon(){ \/\/ Code goes here }"], 0),
+                        new Question("What is JSON?", // Medium Question 1
+                                    ["A programming language",
+                                     "A package for storing Javascript objects",
+                                     "A Javascript event",
+                                     "The company who created Javascript"], 1)];
+/** The questions to be inserted into easy quizes */
+var quiz_easyArray = [new Question("Which of the following is a valid Javascript function declaration?", // Easy Question 0
+                                    ["public void Foo(){ \/\/ Code goes here }",
+                                     "Foo(){ \/\/ Code goes here }",
+                                     "Foo{ \/\/ Code goes here }",
+                                     "function Foo(){ \/\/ Code goes here }"], 3),
+                      new Question("Which of the following is NOT a loop", // Easy Question 1
+                                    ["foreach",
+                                     "for",
+                                     "if",
+                                     "while"], 2),
+                      new Question("What terminology is used when a function is written into a line of code?", // Easy Question 2
+                                    ["The function is called",
+                                     "The function is assigned",
+                                     "The function is performed",
+                                     "The function is calculated"], 0),
+                      new Question("What does a script refer to?", // Easy Question 3
+                                    ["A Javascript function",
+                                     "A Javascript file",
+                                     "A Javascript comment",
+                                     "A Javascript delegate"], 1),
+                      new Question("What best describes the reason Javascript is used?", // Easy Question 4
+                                    ["To display text and images",
+                                     "To get user input",
+                                     "To dynamically alter page elements",
+                                     "To improve the pages' aesthetics"], 2),
+                      new Question("Which option describes how to assign a string to a variable in Javascript?", // Easy Question 6
+                                     ["string x = \"\";",
+                                      "x = \"\";",
+                                      "var x = \"\";",
+                                      "new String(); "], 2),
+                      new Question("What does DOM stand for?", // Easy Question 7
+                                     ["Document Object Module",
+                                      "Document Oriented Model",
+                                      "Design Oriented Model",
+                                      "Document Object Model"], 3),
+                      new Question("Which tag displays the largest text?", // Easy Question 8
+                                     ["<h1>",
+                                      "<title>",
+                                      "<p>",
+                                      "<h4>"], 0),
+                      new Question("Which of the follwing is NOT a valid html attribute?", // Easy Question 9
+                                     ["class",
+                                      "id",
+                                      "href",
+                                      "parent"], 3),
+                      new Question("", // Easy Question 9
+                                     ["",
+                                      "",
+                                      "",
+                                      ""], 3)];
 
+// ========== TEST FUNCTIONS ==========
+
+/**
+ * Loads a test question for debugging purposes
+ */
+function TestQuestion(){
+
+}
+
+/**
+ * Initializes the quiz and displays the first 
+ * 
+ * 
+ * 
+ * 
+ */
+function InitQuiz(a_difficulty, a_number){
+        // Create an array of questions
+        questionArray = Array.from(GenerateQuiz(a_difficulty,a_number));
+
+        // Assign the appropriate ids and create the nav items
+        for(let i = 0; i < questionArray.length; i++){
+            questionArray[i].m_id = i;
+            AddQuestionItem(i);
+        }
+        
+        EnableScrollableElement($(".n-question-list"));
+        DisplayQuestion(questionArray[0]);
+}
